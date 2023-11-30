b39b49379796f532e7a567d81b50761f83aefd45
ammar <ammarkhan575@gmail.com>
2023-12-01 00:44:11 +0530

    implemented nodemailer

--- controllers/fileUpload.js
+++ controllers/fileUpload.js
@@ -5,13 +5,23 @@ const File = require('../models/File');
 
 exports.localFileUpload = async (req, res) => {
     try{
-        const file = req.files.file;
+        const file = req.files.ImageFile;
 
+        // create path where file needs to be stored on server
         const path = __dirname + "/files/"+Date.now() + `.${file.name.split('.')[1]}`;
 
+        // move file file to the path
         file.mv(path,(err)=>{
             console.log(err);
         });
+
+        const {name,tags,email} = req.body;
+        const newFile = new File({
+            name,tags,email,
+            imageUrl:path
+        });
+
+        await newFile.save();
 
         res.json({
             success: true,
--- models/File.js
+++ models/File.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const transporter = require('../config/nodemailer');
 
 const fileSchema = new mongoose.Schema({
     name: {
@@ -16,6 +17,27 @@ const fileSchema = new mongoose.Schema({
         required: true
     }
 });
+
+fileSchema.post('save', async function(doc){
+    try{
+        
+        // send mail
+        const info = await transporter.sendMail({
+            from: "Mohd Ammar",
+            to: doc.email,
+            subject: 'New File upload on cloudinary',
+            html:`<h2>Hello this mail is from cloudinary</h2>`
+        });
+        
+
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Error occur while post middleware of fileSchema"
+        })
+    }
+})
 
 const File = mongoose.model('File', fileSchema);
 
