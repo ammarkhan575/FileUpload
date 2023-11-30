81885bbc33cae6b3b3a9cf6d43efbfb57e53f598
ammar <ammarkhan575@gmail.com>
2023-11-30 14:10:45 +0530

    File model created

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
+// import modules
 const express = require('express');
 const dotenv = require('dotenv');
 
+// import functions
+const connect = require('./config/db');
+const cloudinaryConnect = require('./config/cloudinary');
+
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
+// DB connection
+connect();
+
+// cloudinary connection
+cloudinaryConnect();
+
+// middleware
+app.use(express.json());
+const fileupload = require('express-fileupload');
+app.use(fileupload());
+
+// routes
+const Upload = require('./routes/FileUplaod');
+app.use('/api/v1/upload',Upload);
+
+// activating app
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`);
 });
